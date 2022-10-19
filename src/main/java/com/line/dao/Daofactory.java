45b391a53e9ac53500e8686d13b91728d834568e
cvivis <hsy0754@naver.com>
2022-10-19 13:39:58 +0900

    spring 적용

--- src/main/java/com/line/dao/Daofactory.java
+++ src/main/java/com/line/dao/Daofactory.java
@@ -2,15 +2,22 @@ package com.line.dao;
 
 import com.line.connectionMaker.AwsConnectionMaker;
 import com.line.connectionMaker.ConnectionMaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+//spring은 configuration에서 시작함
+//
 public class Daofactory {
 
+    @Bean
     public hospitalDao hosDaoAws(){
 //        ConnectionMaker connectionMaker = new AwsConnectionMaker();
 //        hospitalDao hospitalDao = new hospitalDao(connectionMaker);
         //아래 한줄로 가능
         return new hospitalDao(new AwsConnectionMaker());
     }
+    @Bean
     public hospitalDao hosDaoLocal(){
 //        ConnectionMaker connectionMaker = new AwsConnectionMaker();
 //        hospitalDao hospitalDao = new hospitalDao(connectionMaker);
--- src/test/java/com/line/dao/hospitalDaoTest.java
+++ src/test/java/com/line/dao/hospitalDaoTest.java
@@ -5,6 +5,11 @@ import com.line.connectionMaker.Dbconnect;
 import com.line.domain.Hospital;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,13 +18,18 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = Daofactory.class)
 class hospitalDaoTest {
 
+    @Autowired
+    ApplicationContext context;
     @Test
     void insertAndSelectId() throws SQLException {
 
 //        AWSUserDaolmpl hospital = new AWSUserDaolmpl();
-        hospitalDao hD = new Daofactory().hosDaoAws();
+//        hospitalDao hD = new Daofactory().hosDaoAws();
+        hospitalDao hD = context.getBean("hosDaoAws", hospitalDao.class);
 
         Hospital hospital = new Hospital("A1101720","서울특별시 강남구 선릉로 669","C","2","강남영상의학과의원");
         hD.insert(hospital);

d740449cc3cef100d2fa0b90216a252dfce145dc
cvivis <hsy0754@naver.com>
2022-10-21 14:20:32 +0900

    insert constructor 로 User 전달

--- src/main/java/userSpring/dao/InsertStrategy.java
+++ src/main/java/userSpring/dao/InsertStrategy.java
@@ -1,12 +1,24 @@
 package userSpring.dao;
 
+import userSpring.model.User;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class InsertStrategy implements StatementStrategy{
+    private User user;
+
+    public InsertStrategy(User user) {
+        this.user = user;
+    }
+
     @Override
     public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
-        return connection.prepareStatement("INSERT INTO users(id, name,password) VALUES (?,?,?)");
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO users(id, name,password) VALUES (?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+        return ps;
     }
 }
--- src/main/java/userSpring/dao/UserDao.java
+++ src/main/java/userSpring/dao/UserDao.java
@@ -1,8 +1,6 @@
 package userSpring.dao;
 
 
-
-
 import org.springframework.dao.EmptyResultDataAccessException;
 import userSpring.model.User;
 
@@ -15,81 +13,81 @@ public class UserDao {
 
     private ConnectionMaker connectionMaker;
 
-    public UserDao(ConnectionMaker connectionMaker){
+    public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
-    public void insert(User user) throws SQLException {
 
-        Connection c = connectionMaker.makeConnection();;
-
-        PreparedStatement ps = null;
-            ps = new InsertStrategy().makePreparedStatement(c);
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
-
-            ps.executeUpdate();
-
-        ps.close();
-
-        System.out.println("insert 완료");
-        c.close();
-    }
-
-    public User selectId(String id) throws SQLException {
-        try{
-            Connection conn = connectionMaker.makeConnection();;
-            PreparedStatement ps2 = conn.prepareStatement("select * from users where id = ?");
-            ps2.setString(1,id);
-            ResultSet result = ps2.executeQuery();
-            User user = null;
-
-            if(result.next()){
-                user = new User(result.getString("id"),result.getString("name"),result.getString("password"));
-            }
-            result.close();
-            ps2.close();
-            if(user == null){
-                throw new EmptyResultDataAccessException(1);
-            }
-
-
-            return user;
-        }catch (SQLException e){
-            throw new RuntimeException(e);
-        }
-
-    }
-    public int deleteAll() throws SQLException {
+    public int jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         Connection conn = null;
         PreparedStatement ps = null;
         try {
             conn = connectionMaker.makeConnection();
-            ps = new DeleteAllStrategy().makePreparedStatement(conn);
+            ps = stmt.makePreparedStatement(conn);
             int result = ps.executeUpdate();
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            if(ps != null){
+        } finally {
+            if (ps != null) {
                 try {
                     ps.close();
                 } catch (SQLException e) {
                 }
             }
-            if(conn != null){
+            if (conn != null) {
                 try {
                     conn.close();
                 } catch (SQLException e) {
                 }
             }
         }
+    }
 
+    public void insert(User user) throws SQLException {
+//
+//        Connection c = connectionMaker.makeConnection();;
+
+//        PreparedStatement ps = null;
+//            ps = new InsertStrategy().makePreparedStatement(c);
+        jdbcContextWithStatementStrategy(new InsertStrategy(user));
+        System.out.println("insert 완료");
+    }
+
+    public User selectId(String id) throws SQLException {
+        try {
+            Connection conn = connectionMaker.makeConnection();
+            ;
+            PreparedStatement ps2 = conn.prepareStatement("select * from users where id = ?");
+            ps2.setString(1, id);
+            ResultSet result = ps2.executeQuery();
+            User user = null;
+
+            if (result.next()) {
+                user = new User(result.getString("id"), result.getString("name"), result.getString("password"));
+            }
+            result.close();
+            ps2.close();
+            if (user == null) {
+                throw new EmptyResultDataAccessException(1);
+            }
+
+
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    public int getCount() throws  SQLException{
+    public int deleteAll() throws SQLException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        int result = jdbcContextWithStatementStrategy(new DeleteAllStrategy());
+        return result;
+    }
+
+    public int getCount() throws SQLException {
         Connection conn = null;
         PreparedStatement ps = null;
 
@@ -98,20 +96,20 @@ public class UserDao {
             ps = conn.prepareStatement("select count(*) from users");
             ResultSet result = ps.executeQuery();
             int count = 0;
-            if(result.next()){
+            if (result.next()) {
                 count = Integer.parseInt(result.getString("count(*)"));
             }
             return count;
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            if(ps != null){
+        } finally {
+            if (ps != null) {
                 try {
                     ps.close();
                 } catch (SQLException e) {
                 }
             }
-            if(conn != null){
+            if (conn != null) {
                 try {
                     conn.close();
                 } catch (SQLException e) {
